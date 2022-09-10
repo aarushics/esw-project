ae8e9661e32d11eb60a5d0f84b9003d27a14515e
priyansh-khunger26 <83164791+priyansh-khunger26@users.noreply.github.com>
2022-09-10 16:57:17 +0530

    Add files via upload

---
+++ ESW/info1.js
@@ -0,0 +1,21 @@
+var decrypted = CryptoJS.AES.decrypt(encrypted, "Secret Passphrase");
+//4d657373616765
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  // TAB
+  // $(document).on('click', '.tab-wrap ul li a', function (e) {
+  //     var curTabContentId = $(this).attr('href');
+  //     $(this).parents('.tab-wrap').find('ul li').removeClass('active');
+  //     $(this).parents('li').addClass('active');
+  //     $(this).parents('.tab-wrap').find('.tab-content-id').removeClass('active');
+  //     $(curTabContentId).addClass("active");
+  //     e.preventDefault();
+  // });
