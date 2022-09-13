54c033ddd5c1811f2fb7368289874d49f18ff88f
priyansh-khunger26 <83164791+priyansh-khunger26@users.noreply.github.com>
2022-09-14 01:08:10 +0530

    Add files via upload

---
+++ esw-project-main/ESW/info1.js
@@ -0,0 +1,32 @@
+// var decrypted = CryptoJS.AES.decrypt(encrypted, "Secret Passphrase");
+//4d657373616765
+  
+  
+// $("#target").load("https://api.thingspeak.com/channels/1579833/fields/1.json?results=2")
+  
+
+// fetch('https://api.thingspeak.com/channels/1579833/feeds.json?results=2')
+//   .then((response) => response.json())
+//   .then((data) => document.getElementById("target").innerHTML = data["feeds"][0]["field1"]);
+
+fetch('https://api.thingspeak.com/channels/1579833/fields/1.json?results=1')
+  .then((response) => response.json())
+  .then((data) => {
+    // var decrypted = CryptoJS.AES.decrypt(data["feeds"][1]["field1"], "Secret Passphrase");
+    document.getElementById("target").innerHTML = decrypted;});
+
+  
+  
+  
+  
+  
+  
+  // TAB
+  // $(document).on('click', '.tab-wrap ul li a', function (e) {
+  //     var curTabContentId = $(this).attr('href');
+  //     $(this).parents('.tab-wrap').find('ul li').removeClass('active');
+  //     $(this).parents('li').addClass('active');
+  //     $(this).parents('.tab-wrap').find('.tab-content-id').removeClass('active');
+  //     $(curTabContentId).addClass("active");
+  //     e.preventDefault();
+  // });
---
+++ esw-project-main/README.md
@@ -0,0 +1,4 @@
+We are showing User Id vitals of fall status and location in case of fall.
+On clicking on the User Id, we are directed to the personal information page of the user.
+The encrypted data is sent from ESP32 and decrypted at the website.
+The User detials are fetched from thingspeak.
